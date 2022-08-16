0c211eb6cfdd24b0274c0ae12f4563e2821e4d56
MarcosViniciusAraujo <m.v.araujo.almeida@gmail.com>
2022-08-15 23:55:47 -0300

    Finalizando o jogo da mega sena

--- lista de exercícios/exercícios obrigatórios 1/ex5.py
+++ lista de exercícios/exercícios obrigatórios 1/ex5.py
@@ -20,4 +20,39 @@ def read_bets(filename):
     return apostas
 
 
+def get_random_result():
+    result = []
+
+    while len(result) <= 6:
+        num_sorteado = random.randint(1, 60)
+
+        if num_sorteado not in result:
+            result.append(num_sorteado)
+    
+    return result
+
+
+
+def check_winner(bets, result):
+
+    for bet in bets:
+        if bet not in result:
+            return False
+    return True
+
+
+
 # Simular sorteio
+
+GABARITO = get_random_result()
+
+aposta = read_bets('apostas16junho.txt')
+
+
+for pessoa, bets in aposta.items():
+    
+    print(f"Jogador: {pessoa}")
+
+    for i,bet in enumerate(bets):
+        is_winner = check_winner(bet, GABARITO)
+        print(f"Aposta #{i + 1}: {is_winner}")
